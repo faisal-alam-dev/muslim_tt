4be5c3f4a12cda0645e18330fe270841d9d9c2c7
faisalalambd <faisal.alamm.bd@gmail.com>
2025-05-07 14:56:35 +0600

    Admin Login Error Notification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,11 @@
                             <div class="card-body">
 
                                 <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+
+                                    @include('widgets.errors')
+
                                     @csrf
+
                                     <div class="form-group">
                                         <label for="email">Email</label>
                                         <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
@@ -56,31 +60,23 @@
                                             Please fill in your email
                                         </div>
                                     </div>
+
                                     <div class="form-group">
                                         <div class="d-block">
                                             <label for="password" class="control-label">Password</label>
-                                            {{-- <div class="float-right">
-                                                <a href="{{ route('password.request') }}" class="text-small">
-                                                    Forgot Password?
-                                                </a>
-                                            </div> --}}
                                         </div>
                                         <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
                                         <div class="invalid-feedback">
                                             please fill in your password
                                         </div>
                                     </div>
-                                    {{-- <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
-                                            <label class="custom-control-label" for="remember-me" name="remember">Remember Me</label>
-                                        </div>
-                                    </div> --}}
+
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
                                             Login
                                         </button>
                                     </div>
+
                                 </form>
 
                             </div>
